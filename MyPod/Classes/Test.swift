21a96d0cc15848d9fd381e333a43bbe6dfb9a4fc
xiaoP <anddygong@gmail.com>
2017-02-26 17:50:07 +0800

    .

---
+++ MyPod/Classes/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  MyPod
+//
+//  Created by xiaoP on 2017/2/26.
+//  Copyright © 2017年 Chicv. All rights reserved.
+//
+
+import Foundation
+
+public func printHelloPod() {
+    print("Hello my pod.")
+}
